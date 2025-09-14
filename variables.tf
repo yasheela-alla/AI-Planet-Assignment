215771f908162224b91e0ee7ec1babf8c76affa8
0xAy4sh <yasheela435@gmail.com>
2025-09-14 21:28:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,27 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "prefect_api_url" {
+  description = "Prefect Cloud API URL"
+  type        = string
+  default     = "https://api.prefect.cloud/api/accounts"
+}
+
+variable "prefect_account_id" {
+  description = "Prefect Cloud Account ID"
+  type        = string
+}
+
+variable "prefect_workspace_id" {
+  description = "Prefect Cloud Workspace ID"
+  type        = string
+}
+
+variable "work_pool_name" {
+  description = "Name of the Prefect work pool"
+  type        = string
+  default     = "ecs-work-pool"
+}
